6f3b36460678b629d421385a7143cff715cc2e25
PHCzack <142808176+PHCzack@users.noreply.github.com>
2025-10-24 08:29:53 +0800

    Add files via upload

---
+++ PHCzackHub.lua
@@ -0,0 +1,6 @@
+--// Load Library
+local CutTree = loadstring(game:HttpGet("https://raw.githubusercontent.com/PHCzack/PHCzackScript/refs/heads/main/CutTree.lua"))()
+
+
+--// Load Library
+local PvB = loadstring(game:HttpGet("https://raw.githubusercontent.com/PHCzack/PHCzackScript/refs/heads/main/Plant%20Vs%20Brainrot.lua"))()
